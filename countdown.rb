a186b63a12797e165b5d88e853427ac6ff3dbff8
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-01-23 12:52:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,13 @@
 #write your code here
 
 def countdown(number)
-  num = 10
-end
+  
+  while number != 0 
+     
+        puts"#{number} SECOND(S)"
+         number-=1 
+         return"HAPPY NEW YEAR"
+ end
+ end
+ 
+ 
